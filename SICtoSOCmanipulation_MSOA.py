aee958c881fbd2294d6c46b572c7c4a1af55efc9
Magda Smith <magda.smith@transportforthenorth.com>
2020-02-11 14:53:20 +0000

    Update SICtoSOCmanipulation_MSOA.py

--- SICtoSOCmanipulation_MSOA.py
+++ SICtoSOCmanipulation_MSOA.py
@@ -3,16 +3,14 @@
 Created on Thu Nov 21 16:10:36 2019
 
 @author: mags15
+
+more info: Y:/NorMITs Land Use/import/NPR Segmentation/DataPrepProcess.doc
+
 """
 
 import os
-import sys
-
-sys.path.append('C:/Users/' + os.getlogin() + '/S/TAME shared resources/Python')
-sys.path.append('C:/Users/' + os.getlogin() + '/S/TAME shared resources/Python')
-sys.path.append('C:/Users/' + os.getlogin() + '/S/NorMITs Utilities/Python')
-
 import pandas as pd
+import nu_project as nup
 
 output = 'emp'
 defaultHomeDir = 'C:/output'
@@ -23,10 +21,126 @@ def SetWd(homeDir = defaultHomeDir, iteration=output):
     return()
     
 # File paths
-msoa_sic = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/sic_div_msoa_2018B.csv')
-#msoa_regTranslation =pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/OA_to_LSOA_to_MSOA_to_LAD_December_2017_Lookup_in_Great_Britain.csv')
+# MSOA divisions HSL data
+msoa_sic = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/sic_div_msoa_2018.csv')
+
+# region splits. Source: HSL. Categories for SIC here are different to HSL data
+ee_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_East of England.csv')
+ne_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_NorthEast.csv')
+se_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_SouthEast.csv')
+yor_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_Yorkshire.csv')
+wal_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_Wales.csv')
+sw_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_SouthWest.csv')
+lon_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_London.csv')
+em_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_East Midlands.csv')
+nw_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_NorthWest.csv')
+scot_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_Scotland.csv')
+wm_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/SIC_West Midlands.csv')
+
+
+Sictrans = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/Regional_SICsplits/CE categories.csv')
 
 SetWd(homeDir = 'C:/NorMITs_Export', iteration=output)
+
+
+# Regionsplits combine
+    
+    nw_splits = nw_splits.rename(columns={'Unnamed: 0': 'Siccat'})
+
+    ee_splits['RGN11nm'] = 'East of England'
+    ne_splits['RGN11nm'] = 'North East'
+    se_splits['RGN11nm'] = 'South East'
+    yor_splits['RGN11nm'] = 'Yorkshire and The Humber'
+    wal_splits['RGN11nm'] = 'Wales'
+    sw_splits['RGN11nm'] = 'South West'
+    lon_splits['RGN11nm'] = 'London'
+    em_splits['RGN11nm'] = 'East Midlands'
+    nw_splits['RGN11nm'] = 'North West'
+    scot_splits['RGN11nm'] = 'Scotland'
+    wm_splits['RGN11nm'] = 'West Midlands'
+
+    frames = [ee_splits, ne_splits, se_splits, yor_splits, wal_splits, sw_splits, 
+          em_splits, lon_splits, nw_splits,scot_splits, wm_splits]
+
+    splits = pd.concat(frames, sort=True)
+    del(wm_splits, scot_splits, nw_splits, em_splits, lon_splits, sw_splits, 
+        wal_splits, yor_splits, se_splits, ne_splits, ee_splits)
+    
+    # get an estimation for high/medium/skilled and apply to the totals
+    splits['higher'] = splits.iloc[:,1:12].sum(axis=1)
+    splits['medium'] = splits.iloc[:,12:22].sum(axis=1)
+    splits['skilled'] = splits.iloc[:,22:26].sum(axis=1)
+    splits['higher'] = splits['higher'].round(1)
+    splits['medium'] = splits['medium'].round(1)
+    splits['skilled'] = splits['skilled'].round(1)
+    
+    # the splits don't add up to 100% so need an uplift so no numbers are missing
+    splits['diff'] = 1-(splits['higher']+splits['medium']+splits['skilled'])
+    splits['higher'] = splits['higher']+(splits['diff']/3)
+    splits['medium'] = splits['medium']+(splits['diff']/3)
+    splits['skilled'] = splits['skilled']+(splits['diff']/3)
+    splits['total'] = splits['higher']+splits['medium']+splits['skilled']
+    splits = splits[['RGN11nm', 'Siccat', 'higher', 'medium', 'skilled']]
+    splits = splits.rename(columns={'Siccat':'CE_SIC'})
+    # now they should add up to 100%
+
+
+    # Melt the HSL MSOA division table
+    sic_columns = msoa_sic.columns[2:]   
+    msoa_sic = msoa_sic.drop(columns = {'RGN11cd'})
+    msoatrans = pd.melt(msoa_sic, id_vars = ['MSOA', 'RGN11nm'], value_vars = sic_columns)
+    msoatrans = msoatrans.rename(columns = {'variable':'HSL_SIC', 'value':'total'})
+    msoatrans['total'].sum()
+    #msoatrans = msoatrans.rename(columns={'Siccat': 'HSL_SIC'})
+    
+    # need to join the CE codes
+    # msoa_sic = msoa_sic.drop(columns = {'RGN11cd'})
+
+    Sictrans = Sictrans.drop(columns = {'CE_SIC_categories'})
+    msoatrans2 = msoatrans.merge(Sictrans, on = 'HSL_SIC', how = 'outer')
+
+    socs = msoatrans2.merge(splits, on = ['RGN11nm', 'CE_SIC'], how = 'outer')
+
+    socs.update(socs.iloc[:, 5:8].mul(socs.total, 0))
+    socs['check']= socs['higher']+socs['medium']+socs['skilled']
+    print(socs['check'].sum())
+
+    socs.to_csv('Y:/NorMITs Land Use/import/NPR Segmentation/UK_SICtoSOCv3.csv')
+    splits.to_csv('Y:/NorMITs Land Use/import/NPR Segmentation/splits.csv')
+    
+# Use TfN Industry sectors weights
+    tfnindsectors = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/raw data and lookups/TfN industrial sector splits.csv')
+    tfnindsectors = tfnindsectors.rename(columns = {'HsL_sIC':'HSL_SIC'})
+    tfnindsectors = tfnindsectors.drop(columns = {'SIC_division', 'Description', 'North sector weights'})
+    indcols = tfnindsectors.columns[1:]
+    print(indcols)
+    tfnindsectors = pd.melt(tfnindsectors, id_vars = 'HSL_SIC', value_vars = indcols)
+    tfnindsectors = tfnindsectors.rename(columns={'variable':'TfN industry', 'value':'splits'})
+    tfnsocs = socs.merge(tfnindsectors, on = 'HSL_SIC', how = 'outer')
+    tfnsocs['splits'] = tfnsocs['splits'].fillna(0)
+    tfnsocs2 = tfnsocs.copy()
+    tfnsocs2.update(tfnsocs2.iloc[:, 5:8].mul(tfnsocs2.splits,0))
+   
+    tfnsocs2['check'] = tfnsocs2['higher']+tfnsocs2['medium']+tfnsocs2['skilled']
+    tfnsocs2['check'].sum()
+    tfnsocs2 = tfnsocs2.drop(columns = {'HSL_SIC', 'check'})
+    tfnind = tfnsocs2[['MSOA', 'TfN industry', 'higher', 'medium', 'skilled']]
+    tfnsocs2 = tfnsocs2.drop(columns = {'RGN11nm', 'total', 'CE_SIC','HSL_SIC', 'splits'})
+
+    tfnind = tfnsocs2.groupby(
+            by = ['MSOA', 'TfN industry'], 
+            as_index = False, 
+            ).sum(axis = 0)
+#.drop(columns = {'splits', 'total'})
+    socs_check =socs[['MSOA', 'check']]
+    socs_check = socs_check.groupby(
+        by = ['MSOA'],
+        as_index = False,
+        ).sum(axis= 0)
+
+"""
+# This is now redundant as it calculates the SIC definitions proposed by CE which are different.
+
 msoa_sic['SIC1'] = msoa_sic['SIC_1']+msoa_sic['SIC_1.1']+msoa_sic['SIC_1.2']
 msoa_sic['SIC2'] = msoa_sic['SIC_2']+msoa_sic['SIC_2.1']+msoa_sic['SIC_2.2']+msoa_sic['SIC_2.3']+msoa_sic['SIC_2.4']
 msoa_sic['SIC3'] = msoa_sic['SIC_3']+msoa_sic['SIC_3.1']+msoa_sic['SIC_3.2']
@@ -83,69 +197,5 @@ msoa_sic = msoa_sic[['MSOA','RGN11nm', 'SIC1', 'SIC2', 'SIC3', 'SIC4', 'SIC5', '
                     'SIC25', 'SIC26', 'SIC27', 'SIC28', 'SIC29', 'SIC30', 'SIC31',
                     'SIC32', 'SIC33', 'SIC34', 'SIC35', 'SIC36', 'SIC37', 'SIC38', 
                     'SIC39', 'SIC40', 'SIC41', 'SIC42', 'SIC43', 'SIC44', 'SIC45']]      
-siccols = msoa_sic.columns[2:]   
+"""
 
-                                   
-ee_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_East of England.csv')
-ne_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_NorthEast.csv')
-se_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_SouthEast.csv')
-yor_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_Yorkshire.csv')
-wal_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_Wales.csv')
-sw_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_SouthWest.csv')
-lon_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_London.csv')
-em_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_East Midlands.csv')
-nw_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_NorthWest.csv')
-nw_splits = nw_splits.rename(columns={'Unnamed: 0': 'Siccat'})
-scot_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_Scotland.csv')
-wm_splits = pd.read_csv('Y:/NorMITs Land Use/import/NPR Segmentation/SIC to SOC/Regional_SICsplits/SIC_West Midlands.csv')
-
-ee_splits['RGN11nm'] = 'East of England'
-ne_splits['RGN11nm'] = 'North East'
-se_splits['RGN11nm'] = 'South East'
-yor_splits['RGN11nm'] = 'Yorkshire and The Humber'
-wal_splits['RGN11nm'] = 'Wales'
-sw_splits['RGN11nm'] = 'South West'
-lon_splits['RGN11nm'] = 'London'
-em_splits['RGN11nm'] = 'East Midlands'
-nw_splits['RGN11nm'] = 'North West'
-scot_splits['RGN11nm'] = 'Scotland'
-wm_splits['RGN11nm'] = 'West Midlands'
-
-frames = [ee_splits, ne_splits, se_splits, yor_splits, wal_splits, sw_splits, 
-          em_splits, lon_splits, nw_splits,scot_splits, wm_splits]
-
-splits = pd.concat(frames, sort=True)
-#splits = splits.replace({'RGN11nm':{'Yorkshire and the Humber':'Yorkshire and The Humber'}})
-del(wm_splits, scot_splits, nw_splits, em_splits, lon_splits, sw_splits, 
-    wal_splits, yor_splits, se_splits, ne_splits, ee_splits)
-
-sic_columns = msoa_sic.columns[2:]
-
-msoatrans = pd.melt(msoa_sic, id_vars = ['MSOA', 'RGN11nm'], value_vars = sic_columns)
-msoatrans = msoatrans.rename(columns = {'variable':'Siccat', 'value':'total'})
-msoatrans['total'].sum()
-
-#get a rough estimation for high/medium/skilled and apply to the totals
-splits['higher'] = splits.iloc[:,1:12].sum(axis=1)
-splits['medium'] = splits.iloc[:,12:22].sum(axis=1)
-splits['skilled'] = splits.iloc[:,22:26].sum(axis=1)
-splits['higher'] = splits['higher'].round(1)
-splits['medium'] = splits['medium'].round(1)
-splits['skilled'] = splits['skilled'].round(1)
-
-splits['diff'] = 1-(splits['higher']+splits['medium']+splits['skilled'])
-splits['higher'] = splits['higher']+(splits['diff']/3)
-splits['medium'] = splits['medium']+(splits['diff']/3)
-splits['skilled'] = splits['skilled']+(splits['diff']/3)
-splits['total'] = splits['higher']+splits['medium']+splits['skilled']
-splits = splits[['RGN11nm', 'Siccat', 'higher', 'medium', 'skilled']]
-# now they add up to 100%
-
-socs = msoatrans.merge(splits, on = ['RGN11nm', 'Siccat'], how = 'outer')
-
-socs.update(socs.iloc[:, 4:7].mul(socs.total, 0))
-socs['check']= socs['higher']+socs['medium']+socs['skilled']
-socs['check'].sum()
-
-socs.to_csv('Y:/NorMITs Land Use/import/NPR Segmentation/UK_SICtoSOCv2.csv')
-splits.to_csv('Y:/NorMITs Land Use/import/NPR Segmentation/splits.csv')
