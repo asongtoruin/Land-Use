1e1730d5535cd5e2f2f7a00120192b65a013d827
Magda Smith <magda.smith@transportforthenorth.com>
2021-03-16 10:57:46 +0000

    Update area_types_fy.py

--- area_types_fy.py
+++ area_types_fy.py
@@ -18,13 +18,15 @@ import matplotlib.colors as colors
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
+import gc
 
 _default_home_dir = 'Y:/NorMITs Land Use/area types/fy_areatypes/'
 _default_landuse_dir = 'Y:/NorMITs Land Use/'
+import_folder = 'Y:/NorMITs Demand/import - test/scenarios/'
+
 _iter = 'fy'
 year = 2050
 
-import_folder = 'Y:/NorMITs Demand/import - test/scenarios/'
 
 def establish_pop_per_area(inputdata, year, scenario):
         
@@ -54,107 +56,99 @@ def calculate_values_per_hectare(inputdata1, inputdata2):
     data['workers_per_hectare'] = data['workers']/data['area_hectares']
     return (data)
  
+def get_scenario_data():    
 # scenario 1
-jam_pop = pd.read_csv(import_folder +'SC01_JAM/population/future_growth_values.csv')
-jam_emp = pd.read_csv(import_folder + 'SC01_JAM/employment/future_growth_values.csv')
-jam_pop = establish_pop_per_area(jam_pop, year,1)
-jam_emp = establish_wor_per_area(jam_emp, year,1) 
-jam_data = calculate_values_per_hectare(jam_pop, jam_emp)
-del(jam_pop, jam_emp)
-#scenario 2
-pp_pop = pd.read_csv(import_folder +'SC02_PP/population/future_growth_values.csv')
-pp_emp = pd.read_csv(import_folder +'SC02_PP/employment/future_growth_values.csv')
-pp_pop = establish_pop_per_area(pp_pop, year,2)
-pp_emp = establish_wor_per_area(pp_emp, year,2) 
-pp_data = calculate_values_per_hectare(pp_pop, pp_emp)
-del(pp_pop, pp_emp)
-# scenario 3
-dd_pop = pd.read_csv(import_folder +'SC03_DD/population/future_growth_values.csv')
-dd_emp = pd.read_csv(import_folder + 'SC03_DD/employment/future_growth_values.csv')
-dd_pop = establish_pop_per_area(dd_pop, year,3)
-dd_emp = establish_wor_per_area(dd_emp, year,3) 
-dd_data = calculate_values_per_hectare(dd_pop, dd_emp)
-del(dd_pop, dd_emp)
-
-# scenario 4
-uzc_pop = pd.read_csv(import_folder +'SC04_UZC/population/future_growth_values.csv')
-uzc_emp = pd.read_csv(import_folder + 'SC04_UZC/employment/future_growth_values.csv')
-uzc_pop = establish_pop_per_area(uzc_pop, year,4)
-uzc_emp = establish_wor_per_area(uzc_emp, year,4) 
-uzc_data = calculate_values_per_hectare(uzc_pop, uzc_emp)
-del(uzc_pop, uzc_emp)
-
-gc.collect()
-#combine them
-data = jam_data.append(pp_data).append(dd_data).append(uzc_data)
+    jam_pop = pd.read_csv(import_folder +'SC01_JAM/population/future_growth_values.csv')
+    jam_emp = pd.read_csv(import_folder + 'SC01_JAM/employment/future_growth_values.csv')
+    jam_pop = establish_pop_per_area(jam_pop, year,1)
+    jam_emp = establish_wor_per_area(jam_emp, year,1) 
+    jam_data = calculate_values_per_hectare(jam_pop, jam_emp)
+    del(jam_pop, jam_emp)
+    #scenario 2
+    pp_pop = pd.read_csv(import_folder +'SC02_PP/population/future_growth_values.csv')
+    pp_emp = pd.read_csv(import_folder +'SC02_PP/employment/future_growth_values.csv')
+    pp_pop = establish_pop_per_area(pp_pop, year,2)
+    pp_emp = establish_wor_per_area(pp_emp, year,2) 
+    pp_data = calculate_values_per_hectare(pp_pop, pp_emp)
+    del(pp_pop, pp_emp)
+    # scenario 3
+    dd_pop = pd.read_csv(import_folder +'SC03_DD/population/future_growth_values.csv')
+    dd_emp = pd.read_csv(import_folder + 'SC03_DD/employment/future_growth_values.csv')
+    dd_pop = establish_pop_per_area(dd_pop, year,3)
+    dd_emp = establish_wor_per_area(dd_emp, year,3) 
+    dd_data = calculate_values_per_hectare(dd_pop, dd_emp)
+    del(dd_pop, dd_emp)
+    
+    # scenario 4
+    uzc_pop = pd.read_csv(import_folder +'SC04_UZC/population/future_growth_values.csv')
+    uzc_emp = pd.read_csv(import_folder + 'SC04_UZC/employment/future_growth_values.csv')
+    uzc_pop = establish_pop_per_area(uzc_pop, year,4)
+    uzc_emp = establish_wor_per_area(uzc_emp, year,4) 
+    uzc_data = calculate_values_per_hectare(uzc_pop, uzc_emp)
+    del(uzc_pop, uzc_emp)
+    
+    gc.collect()
+    #combine them
+    data = jam_data.append(pp_data).append(dd_data).append(uzc_data)
+    return (data)
 
 def classify_areas():
     
-        start_points =  data[(data["msoa_zone_id"] == "E02006917")&(data["scenario"]==1)]
-        start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001063")&(data["scenario"]==1))])
-        start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001050")&(data["scenario"]==1))])
-        start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001036")&(data["scenario"]==1))])
-        
-        data = data.set_index(keys = "msoa_zone_id")
-        start_points = start_points.set_index(keys = "msoa_zone_id")
-        
-        # print(init)
-        
-        start_points = start_points[[
-                "pop_per_hectare",
-                "workers_per_hectare",
-                "flat_pop_percent"
-                ]]
-        
-        north_data = data[(data["north"] == 1) & (data["ntem_area_type_id"] < 5)]
-        
-        north_data = north_data[[
-                "pop_per_hectare",
-                "workers_per_hectare",
-                "flat_pop_percent"
-                ]]
-        
-        full_data = data[[
-                "pop_per_hectare",
-                "workers_per_hectare",
-                "flat_pop_percent"
-                ]]
+    data = get_scenario_data()
+    start_points =  data[(data["msoa_zone_id"] == "E02006917")&(data["scenario"]==1)]
+    start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001063")&(data["scenario"]==1))])
+    start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001050")&(data["scenario"]==1))])
+    start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001036")&(data["scenario"]==1))])
     
+    data = data.set_index(keys = "msoa_zone_id")
+    start_points = start_points.set_index(keys = "msoa_zone_id")
     
-        kmeans = KMeans(n_clusters = 4, init=start_points)
+    # print(init)
     
-        kmeans.fit(north_data)
+    start_points = start_points[[
+            "pop_per_hectare",
+            "workers_per_hectare",
+            "flat_pop_percent"
+            ]]
     
-        print(kmeans.cluster_centers_)
+    north_data = data[(data["north"] == 1) & (data["ntem_area_type_id"] < 5)]
     
-        y_km = kmeans.predict(full_data)
-        full_data["y_km"] = y_km
+    north_data = north_data[[
+            "pop_per_hectare",
+            "workers_per_hectare",
+            "flat_pop_percent"
+            ]]
     
-        area_types = data[["ntem_area_type_id"]]
+    full_data = data[[
+            "pop_per_hectare",
+            "workers_per_hectare",
+            "flat_pop_percent"
+            ]]
+
+
+    kmeans = KMeans(n_clusters = 4, init=start_points)
+
+    kmeans.fit(north_data)
+
+    print(kmeans.cluster_centers_)
+
+    y_km = kmeans.predict(full_data)
+    full_data["y_km"] = y_km
+
+    area_types = data[["ntem_area_type_id"]]
+
+    full_data = pd.merge(
+        full_data,
+        area_types,
+        left_index = True,
+        right_index = True
+        )
+    full_data["tfn_area_type_id"] = np.where(full_data["ntem_area_type_id"]>4,full_data["ntem_area_type_id"], full_data["y_km"]+1)
     
-        full_data = pd.merge(
-            full_data,
-            area_types,
-            left_index = True,
-            right_index = True
-            )
-        #scenario = data[['pop_per_hectare', 'workers_per_hectare','scenario']]
-        
-        #full_data = pd.merge(full_data, scenario, on = ['msoa_zone_id','pop_per_hectare', 'workers_per_hectare'])
-   #     full_data = pd.merge(full_data, y_km, on = )
-         #  full_data2 = pd.merge(
-        #    full_data,
-         #   scenario, 
-         #   left_index = True, right_index = True
-         #   )
-        #full_data["y_km"] = y_km
-        full_data["tfn_area_type_id"] = np.where(full_data["ntem_area_type_id"]>4,full_data["ntem_area_type_id"], full_data["y_km"]+1)
-        
-        #full_data["north"] = data["north"]
-        full_data2 = pd.merge(full_data, data, on =['msoa_zone_id', 'pop_per_hectare', 'workers_per_hectare', 'flat_pop_percent'])
-        full_data2 = full_data2.drop_duplicates()
-        full_data2.to_csv(_default_home_dir+'/full_data_w_scotland_'+_iter+'_'+str(year)+'.csv',
-                         index = True, 
-                         index_label = "msoa_zone_id")
-        
-        return(full_data)
+    full_data2 = pd.merge(full_data, data, on =['msoa_zone_id', 'pop_per_hectare', 'workers_per_hectare', 'flat_pop_percent'])
+    full_data2 = full_data2.drop_duplicates()
+    full_data2.to_csv(_default_home_dir+'/full_data_w_scotland_'+_iter+'_'+str(year)+'.csv',
+                     index = True, 
+                     index_label = "msoa_zone_id")
+    
+    return(full_data)
