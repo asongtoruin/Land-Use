2918bfaf77036cc0af07f7f1986bfd71c4bdb6ac
Magda Smith <magda.smith@transportforthenorth.com>
2021-03-17 15:42:01 +0000

    Update area_types_fy.py
    future years area types are now formatted by msos and outtputed into each scenario folder e.g. Y:\NorMITs Land Use\import\scenarios\SC01_JAM\at_mix

--- area_types_fy.py
+++ area_types_fy.py
@@ -2,16 +2,10 @@
 """
 Created on Wed Mar 10 07:46:51 2021
 
-@author: ESRIAdmin
+@author: mags15
 """
 
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  9 08:38:36 2021
-
-@author: MagdaSmith
-"""
-
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.colors as colors
@@ -19,13 +13,18 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 import gc
+import os  
+from functools import reduce
+
+
 
 _default_home_dir = 'Y:/NorMITs Land Use/area types/fy_areatypes/'
+# to write out to Y:/NorMITs Land Use/import/scenarios/SC01_JAM/
 _default_landuse_dir = 'Y:/NorMITs Land Use/'
 import_folder = 'Y:/NorMITs Demand/import - test/scenarios/'
 
 _iter = 'fy'
-year = 2050
+#year = 2040# 2030, 2033. 2050
 
 
 def establish_pop_per_area(inputdata, year, scenario):
@@ -56,7 +55,8 @@ def calculate_values_per_hectare(inputdata1, inputdata2):
     data['workers_per_hectare'] = data['workers']/data['area_hectares']
     return (data)
  
-def get_scenario_data():    
+def get_scenario_data(year):
+# could be replaced with the inputs straight from the EFS process    
 # scenario 1
     jam_pop = pd.read_csv(import_folder +'SC01_JAM/population/future_growth_values.csv')
     jam_emp = pd.read_csv(import_folder + 'SC01_JAM/employment/future_growth_values.csv')
@@ -92,9 +92,9 @@ def get_scenario_data():
     data = jam_data.append(pp_data).append(dd_data).append(uzc_data)
     return (data)
 
-def classify_areas():
+def classify_areas(year):
     
-    data = get_scenario_data()
+    data = get_scenario_data(year)
     start_points =  data[(data["msoa_zone_id"] == "E02006917")&(data["scenario"]==1)]
     start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001063")&(data["scenario"]==1))])
     start_points = start_points.append (data[((data["msoa_zone_id"] == "E02001050")&(data["scenario"]==1))])
@@ -147,8 +147,59 @@ def classify_areas():
     
     full_data2 = pd.merge(full_data, data, on =['msoa_zone_id', 'pop_per_hectare', 'workers_per_hectare', 'flat_pop_percent'])
     full_data2 = full_data2.drop_duplicates()
-    full_data2.to_csv(_default_home_dir+'/full_data_w_scotland_'+_iter+'_'+str(year)+'.csv',
-                     index = True, 
-                     index_label = "msoa_zone_id")
+    #full_data2.to_csv(_default_home_dir+'/full_data_w_scotland_'+_iter+'_'+str(year)+'.csv',
+     #                index = True, 
+      #               index_label = "msoa_zone_id")
+    full_data2.reset_index(inplace=True)
+    full_data2 = full_data2.rename(columns={'Index':'msoa_zone_id'})
+    full_data2 = full_data2[['msoa_zone_id', 'tfn_area_type_id', 'scenario']]
+    full_data2 = full_data2.rename(columns={'tfn_area_type_id':str(year)})
+    #return(full_data)
+    print('Zones are now classified. Writing to scenario folders')
+    scenario1 = full_data2[(full_data2['scenario'] == 1)].drop(columns={'scenario'})
+    scenario1.to_csv('Y:/NorMITs Land Use/import/scenarios/SC01_JAM/at_mix/area_types_'+str(year)+'.csv', index = False)
+    scenario2 = full_data2[(full_data2['scenario'] == 2)].drop(columns={'scenario'})
+    scenario2.to_csv('Y:/NorMITs Land Use/import/scenarios/SC02_PP/at_mix/area_types_'+str(year)+'.csv', index = False)
+    scenario3 = full_data2[(full_data2['scenario'] == 3)].drop(columns={'scenario'})
+    scenario3.to_csv('Y:/NorMITs Land Use/import/scenarios/SC03_DD/at_mix/area_types_'+str(year)+'.csv', index = False)
+    scenario4 = full_data2[(full_data2['scenario'] ==4)].drop(columns={'scenario'})
+    scenario4.to_csv('Y:/NorMITs Land Use/import/scenarios/SC04_UZC/at_mix/area_types_'+str(year)+'.csv', index = False)
     
-    return(full_data)
+    # split by scenario and output into each csv formatted: msoa, year1, year2 under each scenario folder
+    
+def clean_up_combine(target_folder):
+
+    target_dir = os.listdir(target_folder)
+    import_list = [x for x in target_dir]
+
+    at = []
+    for y in import_list:
+        print('Importing ' + y)
+
+        # Import
+        dat = pd.read_csv(target_folder + '/' + y)
+        print(list(dat))
+        # Append to ph
+        at.append(dat)
+
+    at_all = reduce(lambda x, dat : pd.merge(x, dat, on = 'msoa_zone_id'), at)
+
+    #return(at_all)
+    at_all.to_csv(target_folder+'area_types.csv', index = False)
+    
+
+target_folder_sc01 = 'Y:/NorMITs Land Use/import/scenarios/SC01_JAM/at_mix/'
+target_folder_sc02 = 'Y:/NorMITs Land Use/import/scenarios/SC02_PP/at_mix/'
+target_folder_sc03 = 'Y:/NorMITs Land Use/import/scenarios/SC03_DD/at_mix/'
+target_folder_sc04 = 'Y:/NorMITs Land Use/import/scenarios/SC04_UZC/at_mix/'
+
+# run clean up
+clean_up_combine(target_folder_sc01)   
+clean_up_combine(target_folder_sc02)   
+clean_up_combine(target_folder_sc03)   
+clean_up_combine(target_folder_sc04)   
+ 
+ 
+   
+    
+    
