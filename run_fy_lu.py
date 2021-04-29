aee62c45472d6762fb1a76cfcfd8af07f95fbba9
chrisbstorey <58773847+chrisbstorey@users.noreply.github.com>
2021-04-29 14:21:33 +0100

    Update run_fy_lu.py

--- run_fy_lu.py
+++ run_fy_lu.py
@@ -7,11 +7,12 @@ import land_use.future_land_use.fy_lu as fylu
 # future_years = ['2033', '2035', '2040', '2050']
 
 if __name__ == '__main__':
-    scenarios = ['NTEM']
-    future_years = ['2033', '2035', '2050']
+    scenarios = ['SC01_JAM', 'SC02_PP', 'SC03_DD', 'SC04_UZC']
+    future_years = ['2040']
     pop = True
     emp = False
     export = True
+    balance_demographics = True
 
     for scenario in scenarios:
         for fy in future_years:
@@ -23,10 +24,8 @@ if __name__ == '__main__':
 
             # Define run preferences
             if scenario == 'NTEM':
-                balance_demographics = True
                 adjust_area_type = False
             else:
-                balance_demographics = False
                 adjust_area_type = True
             ca_growth_method = 'factor'
             adjust_soc = False
