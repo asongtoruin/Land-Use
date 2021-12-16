34b0537622fbd965bd07f204fc73a4d4f8a12966
adamtebbs <90684098+adamtebbs@users.noreply.github.com>
2021-12-16 15:10:05 +0000

    Commenting the part run instructions back out
    Commenting the part run instructions back out in an attempt to avoid a conflct when merging

--- run_by_lu.py
+++ run_by_lu.py
@@ -4,12 +4,12 @@ import land_use.base_land_use.census_and_by_lu as bylu
 if __name__ == '__main__':
     # lu_run = bylu.CensusYearLandUse(iteration='iter4g')
     lu_run = bylu.BaseYearLandUse(iteration='iter4g')
-    lu_run.state['3.2.1 read in core property data'] = 1
-    lu_run.state['3.2.2 filled property adjustment'] = 1
-    lu_run.state['3.2.3 household occupancy adjustment'] = 1
-    lu_run.state['3.2.4 property type mapping'] = 1
-    lu_run.state['3.2.5 uplifting 2018 population according to 2018 MYPE'] = 1
-    lu_run.state['3.2.6 and 3.2.7 expand NTEM population to full dimensions and verify pop profile'] = 1
+    # lu_run.state['3.2.1 read in core property data'] = 1
+    # lu_run.state['3.2.2 filled property adjustment'] = 1
+    # lu_run.state['3.2.3 household occupancy adjustment'] = 1
+    # lu_run.state['3.2.4 property type mapping'] = 1
+    # lu_run.state['3.2.5 uplifting 2018 population according to 2018 MYPE'] = 1
+    # lu_run.state['3.2.6 and 3.2.7 expand NTEM population to full dimensions and verify pop profile'] = 1
     # lu_run.state['3.2.8 get subsets of worker and non-worker'] = 1
     # lu_run.state['3.2.9 verify district level worker and non-worker'] = 1
     # lu_run.state['3.2.10 adjust zonal pop with full dimensions'] = 1
