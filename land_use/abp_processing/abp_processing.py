fb961ad5ee9e1ed94d572e211450e965edf0ab06
chrisbstorey <58773847+chrisbstorey@users.noreply.github.com>
2023-03-10 15:59:52 +0000

    Create abp_processing.py

---
+++ land_use/abp_processing/abp_processing.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: 10/03/2023
+Updated on:
+
+Original author:
+Last update made by:
+Other updates made by:
+
+File purpose:
+
+"""
+# Built-Ins
+# Some sort of SQL lib
+
+# Third Party
+
+# Local Imports
+# pylint: disable=import-error,wrong-import-position
+# Local imports here
+# pylint: enable=import-error,wrong-import-position
+
+# # # CONSTANTS # # #
+
+# # # CLASSES # # #
+
+
+# # # FUNCTIONS # # #
+"""
+Build a function to hit the PostGres database for a pre-specified region!
+OR Specify SQL query to just get what you need
+Build analysis ready table (joining all of the relevant data)
+"""
+
+"""
+Is there anything we can build that provides added values
+e.g if you have a house point, and a bus stop point, could you fuse in distance to bus stop by house
+(if you're hitting compute problems can we pick the multiprocessing library)
+"""
+
+"""
+Provide a condensed ABP dataset for:
+* Residential land use model
+Anything useful about property classifications, distance to transport infrastructure
+* Non-residential land use model
+Anything about business type, SIC classification, VOA classification (!)
+"""
+
+"""
+Talk to Isaac about using caf.viz libraries to map 'live'
+"""
