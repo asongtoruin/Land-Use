19d81914f5bf669f79f24360d39f4caee927f463
Suzie.Boddy <suzie.boddy@aecom.com>
2024-04-30 17:35:54 +0100

    Constants directory set up with initial geographic definitions. Possibly bring actual caf.core.zoning.ZoningSystem stuff into this as well, for now it is just strings of zoning cache references.

---
+++ TESTING/constants/geographies.py
@@ -0,0 +1,9 @@
+# Define the name of the model zoning to be consistent with the cache file.
+# The model zoning is the zone system the outputs of the population / employment
+# models are intended to be used in at the end.
+MODEL_ZONING = 'lsoa2021'
+
+# Define other zone systems which are defined in the cache which may be used
+# as part of the modelling.
+MSOA = 'msoa2021'
+
