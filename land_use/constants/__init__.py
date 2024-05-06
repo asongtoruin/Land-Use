5281dd149df643d3ef7db9d0d2a4494bfecbf5ce
Adam Ruszkowski <adam.ruszkowski@aecom.com>
2024-05-06 14:49:44 +0100

    Add initial setup for segments
    Initial setup and example custom segments. May yet be renamed etc, but
    the general flow feels okay.
    
    Co-authored-by: Suzie.Boddy <suzie.boddy@aecom.com>

--- land_use/constants/__init__.py
+++ land_use/constants/__init__.py
@@ -1,1 +1,2 @@
 from .geographies import *
+from .segments import *
---
+++ land_use/constants/segments.py
@@ -0,0 +1,92 @@
+from typing import Dict, List
+
+from caf.core.segments import SegmentsSuper, Segment
+
+
+def is_standard_segment(identifier: str) -> bool:
+    """Confirm whether a provided string corresponds to a standard Segment
+
+    Parameters
+    ----------
+    identifier : str
+        String to check
+
+    Returns
+    -------
+    bool
+        True if belonging to a standard segment, else False.
+    """
+    try:
+        SegmentsSuper(identifier)
+        return True
+    except ValueError:
+        return False
+
+
+def split_input_segments(input_segments: List[str]) -> Dict[bool, List[str]]:
+    """Splits a list of input segment strings into standard segments and custom
+
+    Parameters
+    ----------
+    input_segments : List[str]
+        Segment strings to check
+
+    Returns
+    -------
+    Dict[bool, List[str]]
+        Dictionary with two keys - True and False - with a list for each. True
+        corresponds to those segments which are "standard", False corresponds
+        to those which are likely to be custom.
+    """
+    # Could do this with a defaultdict but it's simple enough to not care, and
+    # this way we *definitely* get both keys every time
+    output_dict = {
+        True: [], False: []
+    }
+
+    # Update respective lists
+    for seg_str in input_segments:
+        output_dict[is_standard_segment(seg_str)].append(seg_str)
+
+    return output_dict
+
+
+_CUSTOM_SEGMENT_CATEGORIES = {
+    "h": {
+        1: "Whole house or bungalow: Detached",
+        2: "Whole house or bungalow: Semi-detached",
+        3: "Whole house or bungalow: Terraced",
+        4: "Flat, maisonette or apartment",
+        5: "A caravan or other mobile or temporary structure",
+    },
+    "hr": {
+        1: "Whole house or bungalow: Detached",
+        2: "Whole house or bungalow: Semi-detached",
+        3: "Whole house or bungalow: Terraced",
+        4: "Flat, maisonette or apartment",
+    },
+    "ha": {
+        1: "No adults or 1 adult in household",
+        2: "2 adults in household",
+        3: "3 or more adults in household",
+    },
+    "hc": {
+        1: "Household with no children or all children non-dependent",
+        2: "Household with one or more dependent children",
+    },
+    "car": {
+        1: "No cars or vans in household",
+        2: "1 car or van in household",
+        3: "2 or more cars or vans in household",
+    },
+}
+
+CUSTOM_SEGMENTS = {
+    key: Segment(name=key, values=values) for key, values in _CUSTOM_SEGMENT_CATEGORIES.items()
+}
+
+
+if __name__ ==  '__main__':
+    example = ['p', 'tp', 'TfN', 'm', 'Land-Use', 'g']
+
+    print(split_input_segments(example))
