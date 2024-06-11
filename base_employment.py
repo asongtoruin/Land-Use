43b3e93130629f33d5b570f22976dd0a9055a21f
Simon Kilby <simon.kilby@aecom.com>
2024-06-11 13:01:00 +0100

    Translate BRES 2021 datasets to LSOA 2021

--- base_employment.py
+++ base_employment.py
@@ -8,11 +8,6 @@ from land_use import data_processing
 
 
 def process_bres_2022(config: dict):
-
-    # Get output directory for intermediate outputs from config file
-    OUTPUT_DIR = Path(config["output_directory"])
-    OUTPUT_DIR.mkdir(exist_ok=True)
-
     # Define whether to output intermediate outputs, recommended to not output loads if debugging
     generate_summary_outputs = bool(config["output_intermediate_outputs"])
 
@@ -50,6 +45,24 @@ def process_bres_2022(config: dict):
     print(bres_2022_part_time_employees_2021_lsoa.data.head())
 
 
+def process_bres_2021(config: dict):
+    bres_2021_employees_2011_msoa_sic_2 = read_dvector(
+        config=config, key="bres_2021_employees_sic_2"
+    )
+
+    bres_2021_employment_2011_lsoa_sic_2 = read_dvector(
+        config=config, key="bres_2021_employment_sic_2"
+    )
+
+    bres_2021_employees_2021_lsoa_sic_2 = translate_to_lsoa_2021(
+        bres_2021_employees_2011_msoa_sic_2
+    )
+
+    bres_2021_employement_2021_lsoa_sic_2 = translate_to_lsoa_2021(
+        bres_2021_employment_2011_lsoa_sic_2
+    )
+
+
 def read_dvector(config: dict, key: str):
     return data_processing.read_dvector_data(
         input_root_directory=config["input_root_directory"],
@@ -72,4 +85,10 @@ if __name__ == "__main__":
         r"scenario_configurations\iteration_5\base_employment_config.yml", "r"
     ) as text_file:
         config = yaml.load(text_file, yaml.SafeLoader)
+
+    # Get output directory for intermediate outputs from config file
+    OUTPUT_DIR = Path(config["output_directory"])
+    OUTPUT_DIR.mkdir(exist_ok=True)
+
+    process_bres_2021(config=config)
     process_bres_2022(config=config)
