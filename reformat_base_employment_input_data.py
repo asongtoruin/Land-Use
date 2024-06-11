f7582d531f7b6bfb17451daa81c2ce703383705a
Simon Kilby <simon.kilby@aecom.com>
2024-06-11 14:14:52 +0100

    Separate the parsers from the main functions

--- reformat_base_employment_input_data.py
+++ reformat_base_employment_input_data.py
@@ -19,38 +19,27 @@ INPUT_DIR = Path(r"I:\NorMITs Land Use\2023\import")
 
 
 def main():
-    convert_bres_2021_employees_2_digit_sic()
-    convert_bres_2021_employment_2_digit_sic()
-    convert_bres_2022_lsoa_employment()
+    process_bres_2021_employees_2_digit_sic()
+    process_bres_2021_employment_2_digit_sic()
+    process_bres_2022_lsoa_employment()
 
 
-def convert_bres_2021_employees_2_digit_sic():
+def process_bres_2021_employees_2_digit_sic():
     zoning = geographies.MSOA_EW_2011_NAME
 
     file_path = (
         INPUT_DIR / "BRES2021" / "Employees" / "bres_employees21_msoa11_2digit.csv"
     )
 
-    df = pd.read_csv(file_path, skiprows=8)
-
-    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
-    df = df.drop(columns=["2011 super output area - middle layer"])
-
-    df = df.rename(columns={"mnemonic": zoning})
-    df = df.dropna(subset=[zoning])
+    df = read_bres_2021_employees_2_digit_sic(file_path=file_path, zoning=zoning)
 
     # keep only england and wales
-    df[zoning] = pp.extract_geo_code(df[zoning], scotland=False)
-    df = df.dropna(subset=[zoning])
-
-    df_wide = reformat_for_sic_2_digit_output(
-        df=df, id_vars=[zoning], var_name="sic_2_digit_description"
-    )
+    df_wide = convert_bres_2021_employees_2_digit_sic(df=df, zoning=zoning)
 
     pp.save_preprocessed_hdf(source_file_path=file_path, df=df_wide)
 
 
-def convert_bres_2021_employment_2_digit_sic():
+def process_bres_2021_employment_2_digit_sic():
     zoning = geographies.LSOA_EW_2011_NAME
 
     file_path = (
@@ -60,16 +49,68 @@ def convert_bres_2021_employment_2_digit_sic():
         / "bres_employment21_lsoa11_2digit_sic.csv"
     )
 
-    df = pd.read_csv(file_path, skiprows=8, low_memory=False)
-    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
-
-    mixed_input_col = (
-        "01 : Crop and animal production, hunting and related service activities"
-    )
-    df[mixed_input_col] = pd.to_numeric(df[mixed_input_col], errors="coerce")
-    df = df.dropna(axis=0, subset=[mixed_input_col])
+    df = read_bres_2021_employment_2_digit_sic(file_path=file_path)
 
     # filter rows to just lsoas in england and wales
+    df_wide = convert_bres_2021_employment_2_digit_sic(df=df, zoning=zoning)
+
+    pp.save_preprocessed_hdf(source_file_path=file_path, df=df_wide)
+
+
+def process_bres_2022_lsoa_employment():
+
+    file_path = INPUT_DIR / "BRES2022" / "264980261866694.csv"
+    zoning = geographies.LSOA_EW_2011_NAME
+    segmentation = segments._CUSTOM_SEGMENT_CATEGORIES["big"]
+
+    # may be able to hard code this if we feel the csv won't change
+    tables_and_line_starts = find_contained_tables_and_line_starts(file_path=file_path)
+
+    tables_names_to_wide_df = {}
+
+    for table_type, skiprows in tables_and_line_starts.items():
+        wide_df = process_individual_bres_2022_table(
+            file_path=file_path,
+            skiprows=skiprows,
+            zoning=zoning,
+            segmentation=segmentation,
+        )
+        tables_names_to_wide_df[table_type] = wide_df
+
+    for table_type, wide_df in tables_names_to_wide_df.items():
+        clean_table_type = table_type.replace(" ", "-")
+        pp.save_preprocessed_hdf(
+            source_file_path=file_path, df=wide_df, multiple_output_ref=clean_table_type
+        )
+
+
+def read_bres_2021_employees_2_digit_sic(file_path: Path, zoning: str) -> pd.DataFrame:
+    df = pd.read_csv(file_path, skiprows=8)
+
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+    df = df.drop(columns=["2011 super output area - middle layer"])
+
+    df = df.rename(columns={"mnemonic": zoning})
+    df = df.dropna(subset=[zoning])
+    return df
+
+
+def convert_bres_2021_employees_2_digit_sic(
+    df: pd.DataFrame, zoning: str
+) -> pd.DataFrame:
+    df[zoning] = pp.extract_geo_code(df[zoning], scotland=False)
+    df = df.dropna(subset=[zoning])
+
+    df_wide = reformat_for_sic_2_digit_output(
+        df=df, id_vars=[zoning], var_name="sic_2_digit_description"
+    )
+
+    return df_wide
+
+
+def convert_bres_2021_employment_2_digit_sic(
+    df: pd.DataFrame, zoning: str
+) -> pd.DataFrame:
     df[zoning] = pp.extract_geo_code(df["Area"], scotland=False)
     df = df.drop(columns=["Area"])
     df = df.dropna(subset=[zoning])
@@ -78,7 +119,19 @@ def convert_bres_2021_employment_2_digit_sic():
         df=df, id_vars=[zoning], var_name="sic_2_digit_description"
     )
 
-    pp.save_preprocessed_hdf(source_file_path=file_path, df=df_wide)
+    return df_wide
+
+
+def read_bres_2021_employment_2_digit_sic(file_path: Path) -> pd.DataFrame:
+    df = pd.read_csv(file_path, skiprows=8, low_memory=False)
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+
+    mixed_input_col = (
+        "01 : Crop and animal production, hunting and related service activities"
+    )
+    df[mixed_input_col] = pd.to_numeric(df[mixed_input_col], errors="coerce")
+    df = df.dropna(axis=0, subset=[mixed_input_col])
+    return df
 
 
 def reformat_for_sic_2_digit_output(
@@ -102,33 +155,6 @@ def reformat_for_sic_2_digit_output(
     return df_wide
 
 
-def convert_bres_2022_lsoa_employment():
-
-    file_path = INPUT_DIR / "BRES2022" / "264980261866694.csv"
-    zoning = geographies.LSOA_EW_2011_NAME
-    segmentation = segments._CUSTOM_SEGMENT_CATEGORIES["big"]
-
-    # may be able to hard code this if we feel the csv won't change
-    tables_and_line_starts = find_contained_tables_and_line_starts(file_path=file_path)
-
-    tables_names_to_wide_df = {}
-
-    for table_type, skiprows in tables_and_line_starts.items():
-        wide_df = process_bres_table(
-            file_path=file_path,
-            skiprows=skiprows,
-            zoning=zoning,
-            segmentation=segmentation,
-        )
-        tables_names_to_wide_df[table_type] = wide_df
-
-    for table_type, wide_df in tables_names_to_wide_df.items():
-        clean_table_type = table_type.replace(" ", "-")
-        pp.save_preprocessed_hdf(
-            source_file_path=file_path, df=wide_df, multiple_output_ref=clean_table_type
-        )
-
-
 def find_contained_tables_and_line_starts(file_path: Path) -> dict[str, int]:
     tables_and_line_starts = {}
 
@@ -143,7 +169,7 @@ def find_contained_tables_and_line_starts(file_path: Path) -> dict[str, int]:
     return tables_and_line_starts
 
 
-def process_bres_table(
+def process_individual_bres_2022_table(
     file_path: Path, skiprows: int, zoning: str, segmentation: dict[int, str]
 ) -> pd.DataFrame:
     # feels like this could be a global parameter or derived from an lsoa list
