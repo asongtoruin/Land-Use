523742deafb998629b4f5678adce4d74c3656e4d
Simon Kilby <simon.kilby@aecom.com>
2024-08-29 16:28:15 +0100

    Incorporate soc 4 in employment

--- base_employment.py
+++ base_employment.py
@@ -63,6 +63,12 @@ wfj = data_processing.read_dvector_from_config(
     key='wfj'
 )
 
+soc_4_factors = data_processing.read_dvector_from_config(
+    config=config,
+    data_block=block,
+    key='soc_4_factors'
+)
+
 # --- Step 0 --- #
 LOGGER.info('--- Step 0 ---')
 LOGGER.info(
@@ -130,9 +136,13 @@ rehydrated_adj_factors_for_lsoa = (
     )
 )
 
-adj_msoa_2011_2_digit_sic = msoa_2011_2_digit_sic * rehydrated_adj_factors_for_msoa
 
-adj_lsoa_2011_1_digit_sic = lsoa_2011_1_digit_sic * rehydrated_adj_factors_for_lsoa
+# apply proportions and fill in nas with 0 (which will be for the unemployed rows)
+adj_msoa_2011_2_digit_sic = rehydrated_adj_factors_for_msoa * msoa_2011_2_digit_sic
+adj_msoa_2011_2_digit_sic.fillna(0)
+
+adj_lsoa_2011_1_digit_sic = rehydrated_adj_factors_for_lsoa * lsoa_2011_1_digit_sic
+adj_lsoa_2011_1_digit_sic.fillna(0)
 
 
 # --- Step 1 --- #
@@ -165,6 +175,7 @@ data_processing.save_output(
         dvector_dimension='jobs'
 )
 
+
 # --- Step 3 --- #
 LOGGER.info('--- Step 3 ---')
 LOGGER.info('Convert 1 Digit SIC 2022 BRES data held in LSOA 2011 zoning to 2021 LSOA (Output E3)')
@@ -193,6 +204,24 @@ ons_sic_soc_jobs_lsoa = ons_sic_soc_jobs_lu.translate_zoning(
     check_totals=False
 )
 
+# write to file and then read back in as had issues passing straight through to apply_prop
+
+from caf.core.data_structures import DVector
+ons_sic_soc_jobs_lsoa.save(Path("ons_sic_soc_jobs_lsoa.hdf"))
+lsoa_2021_1_digit_sic.save(Path("lsoa_2021_1_digit_sic.hdf"))
+
+data_dir = Path(r"C:\OneDrive\OneDrive - AECOM\Desktop\TfN")
+ons_sic_soc_jobs_lsoa_path = Path(data_dir / "ons_sic_soc_jobs_lsoa.hdf")
+ons_sic_soc_jobs_lsoa = DVector.load(ons_sic_soc_jobs_lsoa_path)
+
+lsoa_2021_1_digit_sic_path = Path(data_dir / "lsoa_2021_1_digit_sic.hdf")
+lsoa_2021_1_digit_sic = DVector.load(lsoa_2021_1_digit_sic_path)
+
+jobs_by_lsoa_with_soc_group = data_processing.apply_proportions(
+    ons_sic_soc_jobs_lsoa, lsoa_2021_1_digit_sic
+)
+
+
 # Note a warning is generated here about combinations with SOC as 4. We can ignore it.
 LOGGER.info(f'Applying SOC group proportions to BRES 1-digit SIC jobs')
 jobs_by_lsoa_with_soc_group = data_processing.apply_proportions(
@@ -208,12 +237,67 @@ lsoa_2021_2_digit_sic_1_splits = msoa_2011_2_digit_sic_1_digit_sic_splits.transl
         check_totals=False
 )
 
+# write then read back in to avoid issue
+lsoa_2021_2_digit_sic_1_splits.save(Path("lsoa_2021_2_digit_sic_1_splits.hdf"))
+lsoa_2021_2_digit_sic_1_splits = DVector.load(Path("lsoa_2021_2_digit_sic_1_splits.hdf"))
+
+
 # Note a warning is generated here about combinations with SOC as 4. We can ignore it.
 LOGGER.info(f'Applying SOC group proportions to BRES 2-digit SIC jobs')
-jobs_by_sic_soc_lsoa = data_processing.apply_proportions(
+jobs_by_sic_soc_lsoa_no_soc_4 = data_processing.apply_proportions(
     source_dvector=lsoa_2021_2_digit_sic_1_splits,
     apply_to=jobs_by_lsoa_with_soc_group
 )
+
+
+totals = jobs_by_sic_soc_lsoa_no_soc_4.add_segments(
+    [constants.CUSTOM_SEGMENTS['total']]).aggregate(['total'])
+
+totals_with_segs = totals.add_segments([
+    SegmentsSuper('sic_1_digit').get_segment(),
+        SegmentsSuper('sic_2_digit').get_segment(),
+        SegmentsSuper('soc').get_segment()]
+).aggregate([
+    'sic_1_digit', 'sic_2_digit', 'soc'])
+
+soc_1_3_totals = totals_with_segs.filter_segment_value('sic_1_digit', -1)
+
+soc_1_3_totals = soc_1_3_totals.add_segments([SegmentsSuper('sic_1_digit').get_segment()])
+
+# TODO: switch to using translate zoning directly, 
+# however currently doesn't work with one row df, needs fix in caf.toolkit
+# soc_4_factors_lsoa = soc_4_factors.translate_zoning(
+#         new_zoning=constants.LSOA_ZONING_SYSTEM,
+#         cache_path=constants.CACHE_FOLDER,
+#         weighting=TranslationWeighting.NO_WEIGHT,
+#         check_totals=True,
+# )
+
+#### workaround start ################################################################
+soc_4_factors_with_soc = soc_4_factors.add_segments(["soc"])
+
+test_lsoa = soc_4_factors_with_soc.translate_zoning(
+        new_zoning=constants.LSOA_ZONING_SYSTEM,
+        cache_path=constants.CACHE_FOLDER,
+        weighting=TranslationWeighting.NO_WEIGHT,
+        check_totals=False,
+)
+
+soc_4_factors_lsoa = test_lsoa.aggregate(["total"])
+
+soc_4_factors_lsoa.data = soc_4_factors_lsoa.data / len(test_lsoa.data)
+
+#### workaround end ##################################################################
+
+temp = soc_4_factors_lsoa.add_segments(["sic_2_digit", "soc", "sic_1_digit"])
+temp2 = temp.aggregate(["sic_2_digit", "soc", "sic_1_digit"])
+temp3 = temp2.filter_segment_value("sic_2_digit", -1)
+
+
+soc_4_row = soc_1_3_totals * temp3
+
+jobs_by_sic_soc_lsoa = jobs_by_sic_soc_lsoa_no_soc_4.concat(soc_4_row)
+
 
 # save output to hdf and csvs for checking
 data_processing.save_output(
@@ -225,7 +309,7 @@ data_processing.save_output(
 
 LOGGER.info(f'Uplifting Output E4 to Workforce jobs (WFJ) levels by region (Output E4_2)')
 
-output_e4_by_rgn = jobs_by_sic_soc_lsoa.translate_zoning(
+output_e4_by_rgn = jobs_by_sic_soc_lsoa_no_soc_4.translate_zoning(
     new_zoning=constants.RGN_ZONING_SYSTEM,
     cache_path=constants.CACHE_FOLDER,
     weighting=TranslationWeighting.SPATIAL,
@@ -257,7 +341,7 @@ rehydrated_adj_factors_for_e4_2 = (
     )
 )
 
-output_e4_2 = jobs_by_sic_soc_lsoa * rehydrated_adj_factors_for_e4_2
+output_e4_2 = jobs_by_sic_soc_lsoa_no_soc_4 * rehydrated_adj_factors_for_e4_2
 
 data_processing.save_output(
         output_folder=OUTPUT_DIR,
@@ -267,7 +351,7 @@ data_processing.save_output(
 )
 
 LOGGER.info('--- Step 5 ---')
-LOGGER.info(f'Combining Output E1 and E4 to give Jobs by LSOA SIC 4 digit and SOC group (1-3) (Output E5)')
+LOGGER.info(f'Combining Output E1 and E4 to give Jobs by LSOA SIC 4 digit and SOC group (1-4) (Output E5)')
 # Output E5
 e1_with_sic_2_lad = lad_4_digit_sic.translate_segment(
     from_seg=SegmentsSuper('sic_4_digit').get_segment(),
@@ -284,7 +368,7 @@ e1_with_sic_2_lsoa = e1_with_sic_2_lad.translate_zoning(
 
 jobs_by_sic_2_4_soc_lsoa = data_processing.apply_proportions(
     source_dvector=e1_with_sic_2_lsoa, 
-    apply_to=jobs_by_sic_soc_lsoa
+    apply_to=jobs_by_sic_soc_lsoa_no_soc_4
 )
 
 data_processing.save_output(
